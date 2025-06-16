1ad665343d3bf69ba7275ef94f372a0b7926a0f4
Max Mingxuan Xu <123030795+max177777@users.noreply.github.com>
2025-06-15 20:32:05 -0700

    Update Final-test.py

--- Final-test.py
+++ Final-test.py
@@ -363,12 +363,12 @@ elif tab_selection == "Comparison Tool":
         col1, col2 = st.columns(2)
         with col1:
             st.subheader("Location A")
-            country_a = st.selectbox("Country A", sorted(df_ch4['iso3_country'].unique()), key='loc_a_country')
+            country_a = st.selectbox("Country A", SMAC_COUNTRIES_FULL, key='loc_a_country')
             location_a = st.selectbox("Location A", sorted(df_ch4[df_ch4['iso3_country'] == country_a]['location'].unique()), key='loc_a')
 
         with col2:
             st.subheader("Location B")
-            country_b = st.selectbox("Country B", sorted(df_ch4['iso3_country'].unique()), key='loc_b_country')
+            country_b = st.selectbox("Country B", SMAC_COUNTRIES_FULL, key='loc_b_country')
             location_b = st.selectbox("Location B", sorted(df_ch4[df_ch4['iso3_country'] == country_b]['location'].unique()), key='loc_b')
 
         def get_comparison_df(country, location):
